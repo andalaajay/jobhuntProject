053cc467a92f63164757b2f2eda4da39c539e497
andalaajay <112687977+andalaajay@users.noreply.github.com>
2023-04-05 19:32:18 +0530

    Delete watchList directory

--- watchList/src/main/java/com/job/watchlist/exception/JobAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.job.watchlist.exception;
-
-public class JobAlreadyExistException extends Exception{
-    public JobAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- watchList/src/main/java/com/job/watchlist/exception/NoJobFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.job.watchlist.exception;
-
-public class NoJobFoundException extends Exception{
-    public NoJobFoundException (String message) {
-        super(message);
-    }
-}
